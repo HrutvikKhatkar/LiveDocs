54e05b58f6501c01eb67922af22b9f27eece161c
HrutvikKhatkar <hrutvikkhatkar25@gmail.com>
2025-04-01 10:23:28 +0530

    share setup

--- components/Collaborator.tsx
+++ components/Collaborator.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 import UserTypeSelector from "./UserTypeSelector";
 import { Button } from "./ui/button";
+import { removeCollaborator, updateDocumentAccess } from "@/lib/actions/room.actions";
 
 const Collaborator = ({
   roomId,
@@ -13,8 +14,24 @@ const Collaborator = ({
   const [userType, setUserType] = useState(collaborator.userType || "viewer");
   const [loading, setLoading] = useState(false);
 
-  const shareDocumentHandler = async (type: string) => {};
-  const removeCollaboratorHnadler = async (email: string) => {};
+  const shareDocumentHandler = async (type: string) => {
+    setLoading(true);
+
+    await updateDocumentAccess({
+      roomId,email,
+      userType: type as UserType, 
+      updatedBy:user
+    })
+
+    setLoading(false);
+  };
+  const removeCollaboratorHnadler = async (email: string) => {
+    setLoading(false);
+
+    await removeCollaborator({roomId, email});
+
+    setLoading(true);
+  };
 
   return (
     <li className="flex items-center justify-between gap-2 py-3">
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -6,6 +6,7 @@ import { revalidatePath } from 'next/cache';
 import { parseStringify } from '../utils';
 import { root } from 'postcss';
 import { RoomProvider } from '@liveblocks/react';
+import { redirect } from 'next/navigation';
 
  
 export const createDocument = async ({userId, email}:
@@ -42,11 +43,11 @@ export const getDocument = async ({roomId, userId}: {roomId: string; userId: str
     try{
         const room = await liveblocks.getRoom(roomId);
 
-        // const haveAccess = Object.keys(room.usersAccesses).includes(userId);
+        const haveAccess = Object.keys(room.usersAccesses).includes(userId);
 
-        // if(!haveAccess){
-        //     throw new Error('You do not have access to this document');
-        // }
+        if(!haveAccess){
+            throw new Error('You do not have access to this document');
+        }
         return parseStringify(room);
     }catch(error){
         console.log(`Error happened while getting a room: ${error}`);
@@ -78,3 +79,70 @@ export const getDocuments = async (email:string) => {
         console.log(`Error happened while getting a rooms: ${error}`);
     }
 }
+
+
+export const updateDocumentAccess = async ({ roomId, email, userType, updatedBy }: ShareDocumentParams) => {
+    try {
+      const usersAccesses: RoomAccesses = {
+        [email]: getAccessType(userType) as AccessType,
+      }
+  
+      const room = await liveblocks.updateRoom(roomId, { 
+        usersAccesses
+      })
+  
+      if(room) {
+        const notificationId = nanoid();
+  
+        await liveblocks.triggerInboxNotification({
+          userId: email,
+          kind: '$documentAccess',
+          subjectId: notificationId,
+          activityData: {
+            userType,
+            title: `You have been granted ${userType} access to the document by ${updatedBy.name}`,
+            updatedBy: updatedBy.name,
+            avatar: updatedBy.avatar,
+            email: updatedBy.email
+          },
+          roomId
+        })
+      }
+  
+      revalidatePath(`/documents/${roomId}`);
+      return parseStringify(room);
+    } catch (error) {
+      console.log(`Error happened while updating a room access: ${error}`);
+    }
+  }
+  
+  export const removeCollaborator = async ({ roomId, email }: {roomId: string, email: string}) => {
+    try {
+      const room = await liveblocks.getRoom(roomId)
+  
+      if(room.metadata.email === email) {
+        throw new Error('You cannot remove yourself from the document');
+      }
+      
+      const updatedRoom = await liveblocks.updateRoom(roomId, {
+        usersAccesses: {
+          [email]: null
+        }
+      })
+      
+      revalidatePath(`/documents/${roomId}`);
+      return parseStringify(updatedRoom);
+    } catch (error) {
+      console.log(`Error happened while removing a collaborator: ${error}`);
+    }
+  }
+  
+  export const deleteDocument = async (roomId: string) => {
+    try {
+      await liveblocks.deleteRoom(roomId);
+      revalidatePath('/');
+      redirect('/');
+    } catch (error) {
+      console.log(`Error happened while deleting a room: ${error}`);
+    }
+  }
